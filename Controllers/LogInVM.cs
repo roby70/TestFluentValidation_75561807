42b385628d8fd8b52a6a242d5dd519df8b0875ab
Roberto Ferraris <ro.ferraris@gmail.com>
2023-02-27 20:08:35 +0100

    Add project files.

---
+++ Controllers/LogInVM.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace TestFluentValidation.Controllers;
+
+public class LogInVM {
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; } = false;
+
+    public class Validator : FluentValidation.AbstractValidator<LogInVM>
+    {
+        public Validator() {
+            RuleFor(vm => vm.UserName).NotEmpty()
+                .WithMessage("Please provide an UserName.");
+
+            RuleFor(vm => vm.Password).NotEmpty()
+                .WithMessage("Please provide a Password.");
+        }
+    }
+}
